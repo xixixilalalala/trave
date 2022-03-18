9c7eea85d205ef2175026b03b5146c442a4d1278
xixixilalalala <2647880543@qq.com>
2022-03-18 10:13:01 +0800

    add recommend

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -81,6 +81,9 @@
         .icons >>> .swiper-container{
             height: 0
             padding-bottom: 50%
+        .icons{
+            margin-top: .1rem
+        }
         .icon{
             position relative
             overflow: hidden
