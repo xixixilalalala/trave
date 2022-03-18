ff98c8d6aada32903062b43a1355a8e6ad167979
xixixilalalala <2647880543@qq.com>
2022-03-18 08:44:01 +0800

    add icons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div>
+      <home-header />
+      <home-swiper />
+      <home-icons />
+    </div>
 </template>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  
+<script setup lang='ts'>
+import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
+import HomeIcons from './components/Icons.vue'
+  export default{
+    name:'App',
+    components:{
+      HomeHeader,
+      HomeSwiper,
+      HomeIcons
+    }
+  }
+</script>
+  
+<style>
+  
 </style>
---
+++ src/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption"  >
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+    
+<script setup lang='ts'>
+    export default{
+        name:'HomeSwiper',
+        data (){
+            return{
+                swiperOption:{
+                    pagination: 
+                        '.swiper-pagination',
+                    loop:true
+                },
+                swiperList:[{
+                    id:'0001',
+                    imgUrl:"https://tr-osdcp.qunarzz.com/tr-osd-vs-dapp-ugc/img/edbaff9644f3df8109252700f911f437.jpg"
+                },{
+                    id:'0002',
+                    imgUrl:"https://tr-osdcp.qunarzz.com/tr-osd-vs-dapp-ugc/img/eb573103e9890780fce51b524a3d61e4.jpg"
+                }
+                ]
+            }
+        }
+    }
+</script>
+    
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active{
+    background: #fff
+}
+.wrapper{
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+     .swiper-img{
+        width: 100%
+    }
+}
+   
+</style>
