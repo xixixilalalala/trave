6caa4ad0c31a2209c1531035c9142782a2f286f1
xixixilalalala <2647880543@qq.com>
2022-03-19 09:15:18 +0800

    finish city css

--- src/page/city/City.vue
+++ src/page/city/City.vue
@@ -2,17 +2,23 @@
 <div>
     <city-header />
     <city-search />
+    <city-list />
+    <city-alphabet />
 </div>
 </template>
 
 <script setup lang='ts'>
     import CityHeader from './components/Header.vue'
     import CitySearch from './components/Search.vue'
+    import CityList from './components/List.vue'
+    import CityAlphabet from './components/Alphabet.vue'
     export default {
         name: 'City',
         components:{
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
